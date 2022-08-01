5cc2bff98c6699c6f80a1801afe961d8ddd40ee2
Jordanceldridge13 <Jordan@codedifferently.com>
2022-08-01 14:58:34 +0000

    bday setup

---
+++ birthday-reminder-app-setup/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './Components/data';
+import List from './Components/List';
+
+
+function App() {
+  return <h2>reminder project setup</h2>;
+}
+
+export default App;
